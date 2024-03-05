c4d73c6b90b543f8ec947b11031c8bb35f341a98
Jonathan Claggett <jonathan@claggett.org>
2024-03-04 21:36:45 -0500

    Add docstrings

--- js/src/pathref.js
+++ js/src/pathref.js
@@ -49,17 +49,31 @@ export const pathRefToArray = (x) =>
     ? derefPathRef(x)
     : x
 
+/**
+ * Return a string representing a pathRef. Useful for printing but not safe to
+ * evaluate since it assumes each element of the path can be a dotted literal
+ * and numbers, in particular, are not viable. the returned pathRef will be a
+ * subpath of the it.
+ */
 export const pathRefToString = (x) =>
   isPathRef(x)
     ? ['$']
         .concat(derefPathRef(x))
         .join('.')
     : x
-
+/**
+ * Return a pathRef equivilent to the given array. If `pathRef` is specified,
+ * the returned pathRef will be a subpath of the it.
+ */
 export const arrayToPathRef = ([name, ...path], pathRef = $) =>
   (name == null)
     ? pathRef
     : arrayToPathRef(path, pathRef[name])
 
+/**
+ * Return an array with the same elements as those contained in `x`. The
+ * returned array will be cached as a pathRef and so will be identical (i.e.,
+ * `x===y`) to other pathRef arrays with the same values.
+ */
 export const arrayViaPathRef = (x, pathRef = $) =>
   pathRefToArray(arrayToPathRef(x, pathRef))

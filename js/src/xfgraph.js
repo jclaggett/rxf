dfb825eeaba1b35170d79dd4271ea1a98ebeb624
Jonathan Claggett <jonathan@claggett.org>
2024-03-05 08:58:31 -0500

    composeGraph expects root and leaf path refs
    Similar to walkGraph, composeGraph is now explicitly told the root and
    leaf paths
    
    xfgraph now calculates root and leaf paths when calling composeGraph

--- js/src/xfgraph.js
+++ js/src/xfgraph.js
@@ -5,57 +5,81 @@
 import { transducer, STEP } from './reducing.js'
 import { map, tag, detag, multiplex, demultiplex } from './xflib.js'
 import { $ } from './pathref.js'
-import { graph, walk } from './graph.js'
+import { graph, walkGraph } from './graph.js'
 import { identity, compose } from './util.js'
 
-// Walk a graph of transducers using `multiplex` and `demultiplex` to combine
-// idividual transducers into a 'reduced' set of transducers. Use `leafFn` to
-// provide zero or more sink transducers (i.e., causes side effects.). Use
-// `rootFn` to provide zero or more source transducers (i.e., responds to side
-// effects.). The source transducers returned are also assumed to be defined
-// with asynchronous STEP functions.
-export const composeGraph = (g, { rootFn, leafFn }) =>
-  walk(g, (xfs, node, { root, leaf, path, parentPaths }) => {
-    // Stage 1: leaf nodes
-    if (leaf) {
-      xfs = leafFn(path, node)
-    } else {
-      xfs = xfs.flatMap(identity)
-    }
+/**
+ * Walk a graph of transducers using `multiplex` and `demultiplex` to combine
+ * idividual transducers into a 'reduced' set of transducers. Use `leafFn` to
+ * provide zero or more sink transducers (i.e., causes side effects.). Use
+ * `rootFn` to provide zero or more source transducers (i.e., responds to side
+ * effects.). The source transducers returned are also assumed to be defined
+ * with asynchronous STEP functions.
+ */
+export const composeGraph = (g, { leafFn, leafPathRefs, rootFn, rootPathRefs }) => {
+  return walkGraph(g, rootPathRefs, leafPathRefs,
+    (xfs, node, { root, leaf, path, parentPaths }) => {
+      // Stage 1: leaf nodes
+      if (leaf) {
+        xfs = leafFn(path, node)
+      } else {
+        xfs = xfs.flatMap(identity)
+      }
 
-    // Stage 2: multiplex
-    if ((typeof node === 'function') && (node !== identity) && (xfs.length > 0)) {
-      xfs = [compose(node, multiplex(xfs))]
-    }
+      // Stage 2: multiplex
+      if ((typeof node === 'function') && (node !== identity) && (xfs.length > 0)) {
+        xfs = [compose(node, multiplex(xfs))]
+      }
 
-    // Stage 3: demultiplex
-    if (parentPaths.length > 1) {
-      xfs = xfs.map(xf => compose(demultiplex(parentPaths.length), xf))
-    }
+      // Stage 3: demultiplex
+      if (parentPaths.length > 1) {
+        xfs = xfs.map(xf => compose(demultiplex(parentPaths.length), xf))
+      }
 
-    // Stage 4: root nodes
-    if (root && xfs.length > 0) {
-      const leafXf = multiplex(xfs)
-      xfs = rootFn(path, node).map(rootXf => compose(rootXf, leafXf))
-    }
+      // Stage 4: root nodes
+      if (root && xfs.length > 0) {
+        const leafXf = multiplex(xfs)
+        xfs = rootFn(path, node).map(rootXf => compose(rootXf, leafXf))
+      }
 
-    return xfs
-  }).flatMap(identity)
+      return xfs
+    }).flatMap(identity)
+}
 
-// xfgraph: a transducer constructor that builds a unified transducer from `graph`.
+/**
+ * return a transducer from `g`. This transducer expects a stream of variants
+ * where the 'type' of each variant is associated with any of the root nodes in
+ * the graph (Root nodes have no parents). The stepped values from the
+ * transducer are variants associated with any of the leaf nodes in the graph
+ * (Leaf nodes have no children).
+ */
 export const xfgraph = (g) => {
+  const rootPathRefs = Object.keys(g.nodes)
+    .filter(key => !Object.hasOwn(g.in, key))
+    .map(key => $[key])
+
+  const leafPathRefs = Object.keys(g.nodes)
+    .filter(key => !Object.hasOwn(g.out, key))
+    .map(key => $[key])
+
   const xfs = composeGraph(g, {
     leafFn: ([name], _value) => [tag(name)],
-    rootFn: ([name], _value) => [detag(name)]
+    rootFn: ([name], _value) => [detag(name)],
+    leafPathRefs,
+    rootPathRefs
   })
+
+  console.dir({ xfs })
   return multiplex(xfs)
 }
 
-// mapjoin: return a graph that joins multiple inputs as arguments to `f`.
-// `actives` describes which inputs generate new calls to `f` when new values
-// are received.
+/**
+ * return a graph that joins multiple inputs as arguments to `f`. `actives`
+ * describes which inputs generate new calls to `f` when new values are
+ * received.
+ */
 export const mapjoin = (f, actives) => {
-  const xf = transducer(r => {
+  const joiner = transducer(r => {
     const joined = new Array(actives.length)
     const needed = new Set(actives.keys())
     return {
@@ -65,7 +89,7 @@ export const mapjoin = (f, actives) => {
         let active = actives[i]
         if (needed.has(i)) {
           needed.delete(i)
-          active = true
+          active = true // always active when receiving first needed value
         }
 
         if (active && needed.size === 0) {
@@ -78,7 +102,7 @@ export const mapjoin = (f, actives) => {
 
   return graph({
     ...actives.map((_, i) => map(x => [i, x])),
-    out: xf
+    out: joiner
   },
   actives.map((_, i) => [$[i], $.out]))
 }
